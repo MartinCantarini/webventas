f96ddf98ce674059185b5733eab16cf4c9fa6e6f
Martin Daniel Cantarini <md.cantarini@gmail.com>
2015-09-30 16:50:48 -0300

    commit7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,8 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+  config.assets.compile = true
+  config.assets.digest = true
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
