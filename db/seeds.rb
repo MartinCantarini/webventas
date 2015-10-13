64448e31e0192cba2879e3181ec16b26842bfa5e
Martin Daniel Cantarini <md.cantarini@gmail.com>
2015-10-13 12:07:53 -0300

    first commit entrega3

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
+#   
+	category=Category.new([{name:'Accesorios para Vehículos'},{name:'Animales y Mascotas'},{name:'Antiguedades'},{name:'Arte y Artesanías'},{name:'Bebés'},{name:'Cámaras y accesorios'},{name:'Celulares y teléfonos'},{name:'Coleccionables y Hobbies'},{name:'Computación'},{name:'Consolas y Videojuegos'},{name:'Deportes y Fitness'},{name:'Electrodomésticos'},{name:'Electrónica, Audio y Video'},{name:'Entradas para eventos'},{name:'Hogar, Muebles y Jardín'},{name:'Instrumentos musicales'},{name:'Joyas y relojes'},{name:'Juegos y Juguetes'},{name:'Libros, Revistas y Comics'},{name:'Musica, Peículas y Series'},{name:'Ropa y accesorios'},{name:'Salud y Belleza'}]);
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
