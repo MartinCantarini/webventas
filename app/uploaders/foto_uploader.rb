0612ece91e05f5e9b18fd958f8003431bfc02d46
Martin Daniel Cantarini <md.cantarini@gmail.com>
2015-10-29 19:41:11 -0300

    v1.0 -add novedades home

--- app/uploaders/foto_uploader.rb
+++ app/uploaders/foto_uploader.rb
@@ -40,6 +40,10 @@ include Cloudinary::CarrierWave
      process :resize_to_fill => [300, 300]
    end
 
+  version :novedades do
+     process :resize_to_fill => [150, 150]
+   end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
