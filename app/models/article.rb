745e0bf34c89b46ee0e5ecb39bbee6484c4b2001
Martin Daniel Cantarini <md.cantarini@gmail.com>
2015-11-29 19:18:26 -0300

    --add texticle_full_search

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,25 +9,9 @@ class Article < ActiveRecord::Base
 	validates :ubicacion, presence: true
 	validates :category_id, presence: true
 
-	include PgSearch
-  pg_search_scope :search, against: [:name, :price,:ubicacion,:description],
-    using: {tsearch: {dictionary: "english"}},
-    ignoring: :accents
-
-  def self.text_search(query)
-    if query.present?
-      # search(query)
-      rank = <<-RANK
-        ts_rank(to_tsvector(name), plainto_tsquery(#{sanitize(query)}))
-      RANK
-      where("to_tsvector('english', name) @@ :q or to_tsvector('english', price) @@ :q or to_tsvector('english', ubicacion) @@ :q or to_tsvector('english', description) @@ :q", q: query).order("#{rank} desc")
-    else
-      scoped
-    end
-  end
-	#def self.search(search)
-  	#	where("name @@ :q or price @@ :q or ubicacion @@ :q or description @@ :q", q: search) 
-	#end
+	def self.search(query)
+  		where("name @@ :q or price @@ :q or ubicacion @@ :q or description @@ :q", q: search) 
+	end
 
 	#attr_accessible :foto
 
